8b63db796e9151c4b6143a6a30c8f523a6fe6919
Xavier Stouder <xavier@stouder.io>
2022-08-16 13:44:11 +0200

    fix: rework migrations

--- database/migrations/0000000000002_reports.ts
+++ database/migrations/0000000000002_reports.ts
@@ -11,8 +11,10 @@ export default class extends BaseSchema {
       table.string('password', 255).notNullable()
       table.string('remote_addr', 15).notNullable().index()
       table.string('remote_identity', 255).notNullable()
-      table.integer('id_range').unsigned().references('id').inTable('ranges').notNullable()
       table.integer('id_host').unsigned().references('id').inTable('hosts').notNullable()
+      table.string('country_code', 2).nullable()
+      table.string('as_name', 255).nullable()
+      table.integer('asn', 10).nullable()
       table.timestamp('created_at', { useTz: true })
     })
   }
