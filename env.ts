ebe132f746f1975c5edf2ef7f02b162dc5ec801a
Xavier Stouder <xavier@stouder.io>
2022-10-01 17:12:00 +0200

    fix: ssh host not mandatory

--- env.ts
+++ env.ts
@@ -3,7 +3,6 @@ import Env from '@ioc:Adonis/Core/Env'
 export default Env.rules({
   HOST: Env.schema.string({ format: 'host' }),
   PORT: Env.schema.number(),
-  SSH_HOST: Env.schema.string({ format: 'host' }),
   SSH_PORT: Env.schema.number(),
   APP_KEY: Env.schema.string(),
   NODE_ENV: Env.schema.enum(['development', 'production', 'test'] as const),
